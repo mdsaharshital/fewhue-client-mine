d81ec5f371e3b7fa7fe5ab77cd5d3336b3f4478b
Ashik Ahamed <ashikshamim.007@gamil.com>
2023-03-14 23:09:00 +0600

    handover code for shital

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,27 +1,17 @@
 import React from 'react';
 import { Offcanvas } from 'react-bootstrap';
+import SocialGroup from '../components/SocialGroup/SocialGroup';
 
 const Contact = ({ showContact, handleContactClose }) => {
     return (
-        <Offcanvas className='shop_container' show={showContact} placement='bottom' backdrop={true} scroll={false} onHide={handleContactClose}>
+        <Offcanvas className='contact_canvas' show={showContact} placement='bottom' backdrop={true} scroll={false} onHide={handleContactClose}>
             <Offcanvas.Header closeButton>
-                <Offcanvas.Title className='p-3'></Offcanvas.Title>
+                <Offcanvas.Title className=' fs-1'>
+                    Contact Us
+                </Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
-                <div>
-                    <div className="gallery__text"><span className="gallery__text-inner">Contact</span><span
-                        className="gallery__text-inner text-start">Us</span>
-                    </div>
-                    <div className="contact-form w-50 ms-auto mt-5 shadow">
-                        <div className="pt-5">
-                            <form id='from' className="py-5" >
-                                <input name="name" placeholder="Your name" type="text" required />
-                                <input name="user_email" placeholder="Your email" type="email" required />
-                                <textarea name="message" rows="4" placeholder="Message" type="text" required />
-                                <button type="submit" className="contact_btn">Send<i className="arrow ms-3 fas fa-long-arrow-alt-right"></i></button>                                </form>
-                        </div>
-                    </div>
-                </div>
+                <SocialGroup />
             </Offcanvas.Body>
         </Offcanvas>
     );
