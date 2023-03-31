d902a8abfcbaa96833d8878ffc4d9df33ce73235
mdsaharshital <mdsaharshitol@gmail.com>
2023-03-31 23:53:54 +0600

    update done

--- src/layout/Dashboard/ProductDetails.js
+++ src/layout/Dashboard/ProductDetails.js
@@ -1,40 +1,155 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { Form } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { useGetAllCategoryQuery } from "../../feature/category/categoryApi";
+import {
+  useGetAllProductsQuery,
+  useGetProductPhotoQuery,
+  useUpdateProductMutation,
+} from "../../feature/product/productSlice";
+import { useState } from "react";
+import { photoConverter } from "./../../utils/photoConverter";
+import { toast } from "react-hot-toast";
 
 const ProductDetails = () => {
-    const { id } = useParams();
-    return (
-        <div>
-            Product Details {id}
-            <Form>
-                <Form.Group className="mb-3" >
-                    <Form.Label>Product Name</Form.Label>
-                    <Form.Control type="text" placeholder="Enter Product Name" />
-                </Form.Group>
-                <Form.Group className="mb-3" >
-                    <Form.Label>Product Price</Form.Label>
-                    <Form.Control type="number" placeholder="Enter Product Price" />
-                </Form.Group>
-                <Form.Group className="mb-3" >
-                    <Form.Label>Catagory</Form.Label>
-                    <Form.Control type="text" placeholder="Enter Product Catagory" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Description</Form.Label>
-                    <Form.Control as="textarea" rows={3} />
-                </Form.Group>
-                <Form.Group controlId="formFile" className="mb-3">
-                    <Form.Label>Choose Image</Form.Label>
-                    <Form.Control type="file" />
-                </Form.Group>
-                <Button variant="light" type="submit">
-                    Update
-                </Button>
-            </Form>
-        </div>
-    );
+  const { id } = useParams();
+  const { data: categoryData } = useGetAllCategoryQuery();
+  const { data: picData } = useGetProductPhotoQuery(id);
+  const [selectedCategoryId, setSelectedCategoryId] = useState("");
+  const [updateProduct, { isError, error, isLoading }] =
+    useUpdateProductMutation();
+  const { data } = useGetAllProductsQuery();
+  const selectedProduct = data?.products?.find((product) => product._id === id);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    mode: "onBlur",
+  });
+  const onSubmit = async (formDa) => {
+    console.log("addProduct", formDa.category);
+    const { data } = await updateProduct(formDa);
+    if (isLoading) {
+      toast.loading("Product updating...");
+    }
+    console.log("da", data);
+
+    if (data.success) {
+      toast.success("Product updated successfully ");
+    }
+    if (isError) {
+      toast.error(error);
+    }
+  };
+  //
+  useEffect(() => {
+    if (selectedProduct) {
+      reset(selectedProduct);
+    }
+  }, [selectedProduct, reset]);
+
+  return (
+    <div>
+      <div className="d-flex justify-content-center align-items-center shadow m-4">
+        <h2>
+          Product Details (Current):
+          <h5>Name: {selectedProduct?.name}</h5>
+        </h2>
+        <img src={photoConverter(picData)} className="w-25" alt="" />
+      </div>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Group className="mb-3">
+          <Form.Label>Product Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Product Name"
+            {...register("name", {
+              required: {
+                value: true,
+                message: "Name is Required",
+              },
+            })}
+          />
+          {errors.name?.type === "required" && (
+            <span className="label-text-alt text-danger">
+              {errors.name.message}
+            </span>
+          )}
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Product Price</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder="Enter Product Price"
+            {...register("price", {
+              required: {
+                value: true,
+                message: "price is Required",
+              },
+            })}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Catagory</Form.Label>
+          <Form.Select
+            name="category"
+            placeholder="Select a category"
+            className="form-select mb-3"
+            {...register("category", {
+              required: {
+                value: true,
+                message: "category is Required",
+              },
+            })}
+            value={selectedCategoryId}
+            onChange={(e) => setSelectedCategoryId(e.target.value)}
+          >
+            {/* <option key={selectedProduct._id} value={selectedProduct._id}>
+              {selectedProduct.name}
+            </option> */}
+            {categoryData?.category?.map((c) => (
+              <option key={c._id} value={c._id}>
+                {c.name}
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            {...register("description", {
+              required: {
+                value: true,
+                message: "description is Required",
+              },
+            })}
+          />
+        </Form.Group>
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Label>Choose Image</Form.Label>
+          <Form.Control
+            type="file"
+            {...register("photo", {
+              required: {
+                value: true,
+                message: "photo is Required",
+              },
+            })}
+            accept="image/*"
+          />
+        </Form.Group>
+        <Button variant="light" type="submit">
+          Update Product
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
 export default ProductDetails;
