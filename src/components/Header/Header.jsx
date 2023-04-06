507be4394b381489065f5cd714399ffc2d6e48db
hasanabill <hasanabilna@gmail.com>
2023-04-07 00:38:03 +0600

    checkout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,77 +14,78 @@ import CartSlide from './CartSlide';
 import CheckOut from '../CheckOut/CheckOut';
 import emptyCartImg from '../../assets/emptyCart.png'
 
-const Header = ({handleShowCheckoutClose,handleShowCheckout,showCheckout,  handleShopShow}) => {
+const Header = ({ handleShowCheckoutClose, handleShowCheckout, showCheckout, handleShopShow }) => {
     const [showCart, setShowCart] = useState(false);
     const handleClose = () => setShowCart(false);
     const handleShow = () => setShowCart(true);
     const { cart } = useSelector((auth) => auth.basket);
     const totalPrice = cart.reduce(
-      (acc, product) => acc + (product.price* product.quantity || 0),
-      0
+        (acc, product) => acc + (product.price * product.quantity || 0),
+        0
     );
     console.log("totalPrice", totalPrice);
 
     return (
         <>
-        {/* checkout  canvas */}
-        <Offcanvas className='checkout_modal' show={showCheckout} placement='end' scroll={true} onHide={handleShowCheckoutClose}>
-            <Offcanvas.Header closeButton>
-                <Offcanvas.Title>Checkout</Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-                {/* checkout compo */}
-                <CheckOut totalPrice={totalPrice}/>
-            </Offcanvas.Body>
-         </Offcanvas>
+            {/* checkout  canvas */}
+            <Offcanvas className='checkout_modal' show={showCheckout} placement='end' scroll={true} onHide={handleShowCheckoutClose}>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title className='fs-1'>Checkout</Offcanvas.Title>
+                </Offcanvas.Header>
+                <hr />
+                <Offcanvas.Body>
+                    {/* checkout compo */}
+                    <CheckOut totalPrice={totalPrice} />
+                </Offcanvas.Body>
+            </Offcanvas>
             <Offcanvas className='cart_container' show={showCart} placement='end' scroll={true} onHide={handleClose}>
                 <Offcanvas.Header closeButton>
                     <Offcanvas.Title>Cart</Offcanvas.Title>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
-                {
-                    cart.length >0 ? 
-                    <>
-                    <Swiper
-                    modules={[EffectCoverflow, Pagination, Navigation]}
-                    effect={"coverflow"}
-                    grabCursor={true}
-                    centeredSlides={true}
-                    slidesPerView={"auto"}
-                    coverflowEffect={{
-                    rotate: 0,
-                    stretch: 0,
-                    depth: 100,
-                    modifier: 1,
-                    slideShadows: true,
-                    }}
-                    pagination={true}
-                    navigation
-                    className="mySwiper"
-                >
-                    {cart.map((product) => (
-                    <SwiperSlide productPrice={product.price} productImg={product.photo}>
-                        <CartSlide product={product} />
-                    </SwiperSlide>
-                    ))}
-                </Swiper>
-                <div className="cart_total_container text-center mt-5">
-                    <div className="cart_total_price_box">
-                    <h5>Cart Total</h5>
-                    <p className="price_box">
-                        <img width="15px" height="15px" src={takaIcon} alt="" />
-                        <span className="ms-1">{totalPrice}</span>
-                    </p>
-                    </div>
-                    <button className="checkout_btn" onClick={handleShowCheckout}>Proceed to checkout</button>
-                </div></>
-                    : 
-                    <div className="empty-cart h-full">
-                        <img src={emptyCartImg} alt="" width={'250px'} />
-                        <h1 className='empty_cart_text'>Your cart is empty.</h1>
-                        <button onClick={handleShopShow} className='btn btn-dark btn-lg my-5 '>Shop Now</button>
-                    </div>
-                }
+                    {
+                        cart.length > 0 ?
+                            <>
+                                <Swiper
+                                    modules={[EffectCoverflow, Pagination, Navigation]}
+                                    effect={"coverflow"}
+                                    grabCursor={true}
+                                    centeredSlides={true}
+                                    slidesPerView={"auto"}
+                                    coverflowEffect={{
+                                        rotate: 0,
+                                        stretch: 0,
+                                        depth: 100,
+                                        modifier: 1,
+                                        slideShadows: true,
+                                    }}
+                                    pagination={true}
+                                    navigation
+                                    className="mySwiper"
+                                >
+                                    {cart.map((product) => (
+                                        <SwiperSlide productPrice={product.price} productImg={product.photo}>
+                                            <CartSlide product={product} />
+                                        </SwiperSlide>
+                                    ))}
+                                </Swiper>
+                                <div className="cart_total_container text-center mt-5">
+                                    <div className="cart_total_price_box">
+                                        <h5>Cart Total</h5>
+                                        <p className="price_box">
+                                            <img width="15px" height="15px" src={takaIcon} alt="" />
+                                            <span className="ms-1">{totalPrice}</span>
+                                        </p>
+                                    </div>
+                                    <button className="checkout_btn" onClick={handleShowCheckout}>Proceed to checkout</button>
+                                </div></>
+                            :
+                            <div className="empty-cart h-full">
+                                <img src={emptyCartImg} alt="" width={'250px'} />
+                                <h1 className='empty_cart_text'>Your cart is empty.</h1>
+                                <button onClick={handleShopShow} className='btn btn-dark btn-lg my-5 '>Shop Now</button>
+                            </div>
+                    }
                 </Offcanvas.Body>
             </Offcanvas>
             <div className='header_container fadeIn4'>
