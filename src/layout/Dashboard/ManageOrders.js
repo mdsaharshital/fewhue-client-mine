4f8bf107c492800f39a227f43ba8085ae6099c57
mdsaharshital <mdsaharshitol@gmail.com>
2023-04-11 04:11:27 +0600

    pagination done

--- src/layout/Dashboard/ManageOrders.js
+++ src/layout/Dashboard/ManageOrders.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { ListGroup, Table } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, ListGroup, Table } from "react-bootstrap";
 import {
   useGetAllOrdersQuery,
   useUpdateOrderStatusMutation,
@@ -9,11 +9,18 @@ import { toast } from "react-hot-toast";
 import "./Dashboard.css";
 import { useSelector } from "react-redux";
 import OrderStat from "./Comp/OrderStat";
+import ReactPaginate from "react-paginate";
 
 const ManageOrders = () => {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
   const { data } = useGetAllOrdersQuery();
   const [updateStatus] = useUpdateOrderStatusMutation();
   const { user, isLoading } = useSelector((state) => state.auth);
+  //
+  const pageCount = Math.ceil(data?.length / itemsPerPage);
+  const offset = currentPage * itemsPerPage;
+  const ordersToShow = data?.slice(offset, offset + itemsPerPage);
   //
   const getOrderStatusColor = (status) => {
     switch (status) {
@@ -60,54 +67,75 @@ const ManageOrders = () => {
         <OrderStat orderData={data} />
       </div>
       <h3 className="text-center fs-1 fw-bold my-5">Orders</h3>
-      <Table striped borderless hover responsive>
-        <thead>
-          <tr>
-            <th className="fs-4">#</th>
-            <th className="fs-4">Email</th>
-            <th className="fs-4">Total Price</th>
-            <th className="fs-4">Details</th>
-            <th className="fs-4">Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data?.map((order, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td className={`${getOrderStatusColor(order.status)} fw-bold`}>
-                {order.email}
-              </td>
-              <td>{order.totalPrice}</td>
-              <td>
-                <Link
-                  to={`manage-order/${order._id}`}
-                  className="btn btn-md btn-outline-dark"
-                >
-                  Details
-                </Link>
-              </td>
-              <td>
-                <div className="select-wrapper">
-                  <select
-                    id="status"
-                    name="status"
-                    defaultValue={order.status}
-                    onChange={(e) =>
-                      handleSelectStatus(e.target.value, order._id)
-                    }
+      <Container fluid>
+        <div className="table-container">
+          <Table striped borderless hover responsive>
+            <thead>
+              <tr>
+                <th className="table_font_heading">#</th>
+                <th className="table_font_heading">Email</th>
+                <th className="table_font_heading">Total Price</th>
+                <th className="table_font_heading">Details</th>
+                <th className="table_font_heading">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {ordersToShow?.map((order, index) => (
+                <tr key={offset + index}>
+                  <td>{offset + index + 1}</td>
+                  <td
+                    className={`${getOrderStatusColor(order.status)} fw-bold`}
                   >
-                    <option value="Not Processed">Not Processed</option>
-                    <option value="Processing">Processing</option>
-                    <option value="Shipped">Shipped</option>
-                    <option value="Delivered">Delivered</option>
-                    <option value="Cancelled">Cancelled</option>
-                  </select>
-                </div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+                    {order.email}
+                  </td>
+                  <td>{order.totalPrice}</td>
+                  <td>
+                    <Link
+                      to={`manage-order/${order._id}`}
+                      className="btn btn-md btn-outline-dark"
+                    >
+                      Details
+                    </Link>
+                  </td>
+                  <td>
+                    <div className="select-wrapper">
+                      <select
+                        id="status"
+                        name="status"
+                        defaultValue={order.status}
+                        onChange={(e) =>
+                          handleSelectStatus(e.target.value, order._id)
+                        }
+                      >
+                        <option value="Not Processed">Not Processed</option>
+                        <option value="Processing">Processing</option>
+                        <option value="Shipped">Shipped</option>
+                        <option value="Delivered">Delivered</option>
+                        <option value="Cancelled">Cancelled</option>
+                      </select>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          <ReactPaginate
+            pageCount={pageCount}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={({ selected }) => setCurrentPage(selected)}
+            containerClassName="pagination justify-content-center"
+            pageClassName="page-item"
+            pageLinkClassName="page-link"
+            previousClassName="page-item"
+            previousLinkClassName="page-link"
+            nextClassName="page-item"
+            nextLinkClassName="page-link"
+            activeClassName="active"
+          />
+        </div>
+      </Container>
+      <p className="text-center">Maintain order status carefully</p>
     </div>
   );
 };
